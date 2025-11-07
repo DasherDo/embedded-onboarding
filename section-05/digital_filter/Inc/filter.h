f50a4598324f6912af768f55de2035b88e4f64c8
Josh <josh@hessjd.com>
2025-11-07 14:06:12 -0600

    start digital filter implementation

--- section-05/digital_filter/Inc/filter.h
+++ section-05/digital_filter/Inc/filter.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct
 {
@@ -11,6 +12,18 @@ typedef struct
 	int arr[];
 } buffer;
 
+float kernel[1024] = {1 / 2};
+
 int add_to_buffer(buffer *buf, int val);
+
+int clear_buffer(buffer *buf);
+
+int *get_coefficients(buffer *buf);
+
+int generate_delta();
+
+int generate_sine();
+
+int convolve(buffer *buf);
 
 #endif
